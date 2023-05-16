ba5f4f93ae698ddaad6b4f454b8bf3cbdd2e1643
Matthias Rettl <matthias.rettl@stud.unileoben.ac.at>
2023-05-16 11:39:51 +0200

    :white_check_mark: update tests/test_energy_gradient

--- cf/tests/test_energy_gradient.py
+++ cf/tests/test_energy_gradient.py
@@ -2,92 +2,77 @@ import unittest
 
 import numpy as np
 
-import cf.symbolic_util
 from cf import element_definitions as el_def
 from cf_shared import cf_c
-from cf import expressions as exp
-import Conf_Forces_py as cf_m
+from cf.one_element_runner import simulate_one_element
+from cf_shared.tensor_util import tensor_from_abaqus_notation
 
 
 class TestEnergyGradient(unittest.TestCase):
+    def test_energy_gradient(self):
+        STRAIN = 1e-3*np.array([
+            [1., 0., 0.],
+            [0., 0., 0.],
+            [0., 0., 0.]
+        ])
+        delta = 1e-3
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        # TODO:
-        cls.abaqus_data = one_element_abaqus_runner.simulate_all_element_types()
+        for element_type in el_def.R_at_nodes_of_element.keys():
+            with self.subTest(element_type):
+                X_at_nodes = el_def.R_at_nodes_of_element[element_type]
+                n, d = X_at_nodes.shape
+
+                origin = simulate_one_element(
+                    X_at_nodes=X_at_nodes,
+                    U_at_nodes=X_at_nodes @ STRAIN[:d, :d],
+                    element_type=element_type,
+                    load_name="origin",
+                    folder=f"res/tests/test_energy_gradient/{element_type}"
+                )
+
+                increments = list()
+                for i in range(n):
+                    increment = list()
+                    increments.append(increment)
+                    for j in range(d):
+                        X_at_nodes_inc = np.array(X_at_nodes)
+                        X_at_nodes_inc[i, j] += delta
+
+                        increment.append(simulate_one_element(
+                            X_at_nodes=X_at_nodes_inc,
+                            U_at_nodes=X_at_nodes_inc @ STRAIN[:d, :d],
+                            element_type=element_type,
+                            load_name=f"increment_{i}_{j}",
+                            folder=f"res/tests/test_energy_gradient/{element_type}"
+                        ))
 
 
-    def test_something(self):
-        element_type = "C3D8"
-        data = self.abaqus_data[element_type]
-        origin = data["origin"]
-        gradient = data["gradient"]
+                dALLSE_dX = np.array([
+                    [
+                        (
+                                increment_axis["model"]["ALLSE"]
+                                - origin["model"]["ALLSE"]
+                        ) / delta
+                        for increment_axis in increment
+                    ]
+                    for increment in increments
+                ])
 
-        n, d = np.shape(origin["nodes"]["COORD"])
+                CF = cf_c.compute_CF(
+                    e_at_int_points=np.array(origin["integration_points"]["SENER"]).reshape((1, -1)),
+                    X_at_nodes=[X_at_nodes],
+                    U_at_nodes=[X_at_nodes @ STRAIN[:d, :d]],
+                    S_at_int_points=[tensor_from_abaqus_notation(origin["integration_points"]["S"])[:, :d, :d]],
+                    element_type=element_type,
+                    method="dbf"
+                )[0]
 
-        expected_CF = np.zeros((n, d), dtype=float)
-        for derivative in gradient:
-            dALLSE = np.array(derivative["dALLSE"])
-            dX = np.array(derivative["dX"])
-            i, j = np.argwhere(dX != 0)[0]
-            dALLSE_dXij = dALLSE / dX[i, j]
-
-            expected_CF[i, j] = dALLSE_dXij
-
-        S = np.array(origin["integration_points"]["S"])
-        S_at_integration_points = np.zeros((len(S), 3, 3), dtype=float)
-        S_at_integration_points[:, 0, 0] = S[:, 0]
-        S_at_integration_points[:, 1, 1] = S[:, 1]
-        S_at_integration_points[:, 2, 2] = S[:, 2]
-        S_at_integration_points[:, 0, 1] = S_at_integration_points[:, 1, 0] = S[:, 3]
-        S_at_integration_points[:, 0, 2] = S_at_integration_points[:, 2, 0] = S[:, 4]
-        S_at_integration_points[:, 1, 2] = S_at_integration_points[:, 2, 1] = S[:, 5]
-
-        a = cf_c.compute_static_dbf_CF_for_multiple_C3D8(
-            e_at_int_points=[np.array(origin["integration_points"]["SENER"]).flatten()],
-            X_at_nodes=[origin["nodes"]["COORD"]],
-            U_at_nodes=[origin["nodes"]["U"]],
-            S_at_int_points=[np.zeros_like(S_at_integration_points[:, :3, :3])]
-        ).reshape((n, d))
-
-        actual_CF = cf_c.compute_static_mbf_CF_for_multiple_C3D8(
-            e_at_int_points=[np.array(origin["integration_points"]["SENER"]).flatten()],
-            X_at_nodes=[origin["nodes"]["COORD"]],
-            U_at_nodes=[origin["nodes"]["U"]],
-            S_at_int_points=[S_at_integration_points[:, :3, :3]]
-        ).reshape((n, d))
-
-        markus_CF = cf_m.calc_Conf_Force_C3D8_static(
-            Coords=np.array([origin["nodes"]["COORD"]]),
-            Element_U=np.array([origin["nodes"]["U"]]),
-            S_vec=np.array([S]),
-            PENER=np.array([[0.]*8]),
-            SENER=np.array([[origin["element"]["ESEDEN"]]*8]),
-            method="dbf"
-        )
-        #
-        R = cf.math_util.create_symbolic_matrix("{row}", ["r", "s", "t"], 1)
-        S = cf.math.create_symbolic_matrix("S{row}{col}", 3, 3, *R, is_symmetric=True)
-        H_ = exp.eval_H(
-            R,
-            el_def.R_at_nodes_of_element[element_type],
-            el_def.exponents_of_shape_functions_of_element[element_type]).doit()
-
-        dH_dR_ = exp.eval_dH_dR(H_, R).doit()
-        J_ = exp.eval_J(np.array(origin["nodes"]["COORD"]), dH_dR_).doit()
-        dH_dX_ = exp.eval_dH_dX(dH_dR_, J_).doit()
-        dU_dX_ = exp.eval_dU_dX(np.array(origin["nodes"]["U"]), dH_dX_).doit()
-        F_ = exp.eval_F(3, dU_dX_).doit()
-        P_ = exp.eval_P(F_, S)
-        P_at_integration_points = [
-            exp.eval_P(F_, S_at_integration_point)
-            for S_at_integration_point in S_at_integration_points
-        ]
-
-        e = origin["element"]["ESEDEN"] * np.eye(3, 3)
-        CS = e - F_.T * P_at_integration_points[0]
-
-        self.assertEqual(True, False)  # add assertion here
+                np.testing.assert_array_almost_equal(
+                    dALLSE_dX,
+                    CF,
+                    decimal=3
+                )
+                # TODO:
 
 
 if __name__ == '__main__':
