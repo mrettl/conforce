30ab41d3acf7d7e2ae6c51ec0af4a0e54c0955d6
Matthias Rettl <matthias.rettl@stud.unileoben.ac.at>
2023-05-18 10:19:37 +0200

    :bug: fix import of version in cf_shared/__init__

--- cf_shared/__init__.py
+++ cf_shared/__init__.py
@@ -1,7 +1,7 @@
 """
 This package contains code that runs both in Python 3.* and in Abaqus Python.
 """
-from __version__ import version
+from cf_shared.__version__ import version
 
 project = 'CF - Configurational Forces Plug-in'
 description = 'Compute Configurational Forces using a fast C-Extension'
