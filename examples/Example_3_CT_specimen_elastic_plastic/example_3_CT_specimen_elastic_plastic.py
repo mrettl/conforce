42fd5bf800cac8d45afe3f9dc6c5be5c2baaa3ce
Matthias Rettl <matthias.rettl@stud.unileoben.ac.at>
2023-05-28 20:46:29 +0200

    :art: add Example 3

---
+++ examples/Example_3_CT_specimen_elastic_plastic/example_3_CT_specimen_elastic_plastic.py
@@ -0,0 +1,125 @@
+r"""
+Example 3 - CT-specimen with linear elastic material behaviour
+==============================================================
+
+Kolednik [1]_
+
+>>> w = 50  # mm
+>>> a = 27  # mm
+>>> b = 1  # mm
+>>> b_n = 1  # mm
+>>> nu = 0.3
+>>> youngs_modulus = 200_000 # MPa
+
+.. list-table:: Parameters
+    :header-rows: 1
+
+    * - Parameter
+      - value
+    * - :math:`w`
+      - 50 mm
+    * - :math:`a`
+      - 27 mm
+    * - :math:`H`
+      - 60 mm
+    * - :math:`\nu` (Poisson's ratio)
+      - 0.3
+    * - :math:`E` (Young's modulus)
+      - 200 GPa
+    * - :math:`u`
+      - 0.5 mm
+
+
+Simulation
+----------
+
+>>> import os
+>>> import numpy as np
+>>> HOME_DIR = os.path.abspath(".")
+>>> os.chdir(__file__ + "/..")
+
+
+:py:mod:`example_3_abaqus_script`
+
+>>> import subprocess
+>>> if not os.path.exists("results.json"):
+...     _ = subprocess.call("abaqus cae noGUI=example_3_abaqus_script.py", shell=True)
+
+>>> import json
+>>> with open("results.json", "r", encoding="UTF-8") as fh:
+...     results = json.load(fh)
+
+
+>>> load = results["reaction_force"]
+>>> np.around(load, 3)  # N
+56319.531
+
+Theory
+------
+
+# TODO: wo finde ich die Norm?
+
+>>> geometry_factor = (
+...     (2 + a/w)
+...     * (
+...         0.866
+...         + a/w * 4.64
+...         - (a/w)**2 * 13.32
+...         + (a/w)**3 * 14.72
+...         - (a/w)**4 * 5.6
+...     )
+...     / (1 - a/w)**(3./2)
+... )
+>>> geometry_factor
+10.821389667870234
+
+# TODO: warum muss ich b=bn=25 mm einsetzen, wenn load in 2D N/mm !?!?!?!?
+
+>>> k = (
+...     load
+...      * geometry_factor
+...     / (b * b_n * w) ** 0.5
+... )
+>>> k
+86190.03660880736
+
+>>> j_integral_theory = k**2 * (1 - nu**2) / youngs_modulus
+>>> j_integral_theory
+
+Abaqus J-Integral
+-----------------
+
+>>> j_integral_abaqus = results["J"][57][-1]
+>>> np.around(j_integral_abaqus, 3)
+54.635
+
+Configurational forces
+----------------------
+
+>>> (cfx_in_a, _) = results["CF"]["A"]
+>>> np.around(cfx_in_a, 3)
+-54.484
+
+>>> (cfx_in_b, _) = results["CF"]["B"]
+>>> np.around(cfx_in_b, 3)
+-54.633
+
+>>> (cfx_in_c, _) = results["CF"]["C"]
+>>> np.around(cfx_in_c, 3)
+-32.306
+
+References
+----------
+
+.. [1] Kolednik, Otmar, Ronald Schöngrundner, and Franz Dieter Fischer.
+    "A new view on J-integrals in elastic–plastic materials."
+    International Journal of Fracture 187.1 (2014): 77-107.
+
+Change to home directory
+
+>>> os.chdir(HOME_DIR)
+"""
+import doctest
+
+if __name__ == '__main__':
+    doctest.testmod(verbose=True)
---
+++ examples/Example_3_CT_specimen_elastic_plastic/example_3_abaqus_script.py
@@ -0,0 +1,108 @@
+"""
+This is an Abaqus script for the example in :py:mod:`example_3_CT_specimen_linear_elastic`.
+
+To simulated and write a result.json file open a shell and type:
+
+.. code-block:: console
+
+    abaqus cae noGUI="example_3_abaqus_script.py"
+
+"""
+from __future__ import print_function
+
+import sys
+import os
+import json
+
+import numpy as np
+
+import abaqus as abq
+
+# append path to the folder where conforce_abq is located and import conforce_abq afterward
+sys.path.append(os.path.abspath("../.."))
+from conforce_abq.main import apply
+
+
+def simulate_and_save_results(inp_file_path="CT_specimen_CPE4.inp"):
+    """
+    This function:
+    - simulate the input file
+    - computes the configurational forces in the odb
+    - writes results to a file called "results.json"
+
+    :param inp_file_path: str, path to input file to simulate
+    :return: updated Odb with configurational forces as field output
+    """
+
+    # create a job from the input file, start the simulation and wait until the simulation completed
+    job_name = os.path.basename(inp_file_path).split(".")[0]
+    job = abq.mdb.JobFromInputFile(
+        name=job_name,
+        inputFileName=inp_file_path
+    )
+    job.submit()
+    job.waitForCompletion()
+
+    # open odb with **readOnly=False**
+    odb = abq.session.openOdb(job_name + ".odb", readOnly=False)
+
+    # apply conforce plugin and request configurational forces as field output
+    odb = apply(
+        odb,
+        request_CF=True,
+        CF_name="CONF_FORCE_LIN_EL",
+        method="dbf",
+        e_expression="SENER"
+    )
+
+    # put all results in this dictionary
+    results = dict()
+
+    # extract CF field output
+    fo_CF = odb.steps['Loading'].frames[-1].fieldOutputs["CONF_FORCE_LIN_EL"]
+
+    results["CF"] = dict()
+    for set_name, region in odb.rootAssembly.nodeSets.items():
+        fo_CF_in_region = fo_CF.getSubset(
+            region=region
+        )
+
+        results["CF"][set_name] = np.sum([
+            value.data
+            for value in fo_CF_in_region.values
+        ], axis=0).tolist()
+
+    # extract history outputs
+    (ho_assembly, ho_J_integral, ho_bc_lower, ho_bc_upper) = [
+        history_region.historyOutputs
+        for history_region in odb.steps['Loading'].historyRegions.values()
+    ]
+
+    # extract J-Integrals at the last frame
+    results["J"] = [
+        [key, value.data[-1][-1]]
+        for key, value in ho_J_integral.items()
+    ]
+
+    # extract reaction forces
+    results["reaction_force"] = ho_bc_upper['RF2'].data[-1][-1]
+
+    # save results
+    with open("results.json", "w") as fh:
+        json.dump(results, fh, indent=4)
+
+    return odb
+
+
+if __name__ == '__main__':
+    # delegate output to console
+    stdout_default, stderr_default = sys.stdout, sys.stderr
+    sys.stdout, sys.stderr = sys.__stdout__, sys.__stderr__
+    try:
+        # simulate
+        simulate_and_save_results()
+
+    finally:
+        # restore default output streams
+        sys.stdout, sys.stderr = stdout_default, stderr_default
+
