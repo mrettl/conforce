a360db53050a56da6d6e52227bf9bf85f5cf5f91
Matthias Rettl <matthias.rettl@stud.unileoben.ac.at>
2023-05-29 11:58:31 +0200

    :wrench: split requirements into two requirements files (for doc and installation); add classifiers to setup.py

--- conforce_shared/__init__.py
+++ conforce_shared/__init__.py
@@ -5,6 +5,5 @@ from conforce_shared.__version__ import version
 
 project = 'conforce'
 description = 'Compute Configurational Forces using a fast C-Extension'
-helpUrl = 'https://git.unileoben.ac.at/kkv/cf3d'
 project_copyright = '2023, Matthias Rettl'
 author = 'Matthias Rettl, Markus Tauscher, Sigfried Frankl, Martin Pletz'
--- setup.py
+++ setup.py
@@ -2,31 +2,56 @@ from setuptools import setup
 
 import conforce_shared
 
+
+def read_file(path: str):
+    with open(path, "r") as fh:
+        return fh.read()
+
+
 setup(
     name=conforce_shared.project,
     version=conforce_shared.version,
-    description=conforce_shared.description,
-    url=conforce_shared.helpUrl,
-    project_urls={
-        'Documentation': 'https://cf-configurational-forces-plug-in.readthedocs.io/en/main/',
-        'Source': 'https://github.com/mrettl/ConfigurationalForcesPlugin',
-        'Tracker': 'https://github.com/mrettl/ConfigurationalForcesPlugin/issues',
-    },
-    license_files=["LICENSE.txt"],
     author=conforce_shared.author,
     author_email='matthias.rettl@unileoben.ac.at',
+    description=conforce_shared.description,
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    url='https://cf-configurational-forces-plug-in.readthedocs.io/en/main/',
+    project_urls={
+        'Documentation': 'https://cf-configurational-forces-plug-in.readthedocs.io/en/main/',
+        'Source': 'https://github.com/mrettl/conforce',
+        'Tracker': 'https://github.com/mrettl/conforce/issues',
+    },
+    license=read_file("LICENSE.txt"),
+    license_files=["LICENSE.txt"],
     packages=['conforce', 'conforce_shared'],
     package_dir={'.': ''},
     package_data={'conforce_shared': ['*.dll', '*.so']},
     install_requires=[
-        "numpy~=1.21.5",
-        "sympy~=1.10.1",
+        requirement.strip()
+        for requirement
+        in read_file("requirements.txt").splitlines()
+        if not requirement.startswith("#") or requirement.strip() == ""
     ],
     extras_require={
-        "documentation": [
-            "Sphinx>=5.0",
-            "recommonmark>=0.7",
-            "sphinx_rtd_theme"
+        "doc": [
+            requirement.strip()
+            for requirement
+            in read_file("doc/requirements.txt").splitlines()
+            if not requirement.startswith("#") or requirement.strip() == ""
         ]
     },
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Programming Language :: Python :: 3 :: Only",
+        (
+            "Development Status :: 3 - Alpha" if "-alpha" in conforce_shared.version.lower()
+            else "Development Status :: 4 - Beta" if "-beta" in conforce_shared.version.lower()
+            else "Development Status :: 5 - Production/Stable"
+        )
+    ]
 )
